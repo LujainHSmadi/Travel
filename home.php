cecf9f446d149b5e0d7151cb9e950793912ea75e
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-05-12 23:08:04 +0300

    main

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+     <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+     <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https: //cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file  -->
+    <link rel="stylesheet" href="CSS/style.css">
+
+
+</head>
+<body>
+    <section class= "header">
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+    <a href="home.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="package.php">Package</a>
+    <a href="book.php">Book</a>
+    </nav>
+    </section>
+
+
+
+    <!-- swiper js  -->
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <!-- custom js file  -->
+    <script src="JS/script.js></script>
+</body>
+</html>
